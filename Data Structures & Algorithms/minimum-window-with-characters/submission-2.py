65c709eabdbb3f6f54261bec51f87194fd2284d4
Maleek Hussain Ali <115790326+MaleekNoob@users.noreply.github.com>
2026-09-10 19:01:04 -0500

    Add: minimum-window-with-characters - submission-2

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-2.py
@@ -0,0 +1,36 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        need_count = {}
+        for char in t:
+            need_count[char] = need_count.get(char, 0) + 1
+
+        have_count = {}
+
+        min_len = float('inf')
+        have = 0
+        left = 0
+        need = len(need_count)
+        result = ""
+
+        for right in range(len(s)):
+            have_count[s[right]] = have_count.get(s[right], 0) + 1
+
+            if s[right] in need_count and have_count[s[right]] == need_count[s[right]]:
+                have += 1
+
+            while need == have:
+                # we got valid window, lets shrink
+                wind_len = right - left + 1
+                if wind_len < min_len:
+                    min_len = wind_len
+                    result = s[left: right + 1]
+
+                # remove from left
+                have_count[s[left]] -= 1
+
+                if s[left] in need_count and have_count[s[left]] < need_count[s[left]]:
+                    have -= 1
+
+                left += 1
+
+        return result
